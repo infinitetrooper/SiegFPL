0f7276be42b4d72746353c9d0f4a8fb41f0b873b
Akhil Varma <16632903+infinitetrooper@users.noreply.github.com>
2024-10-06 12:53:52 +0000

    Updated config to host

--- app.py
+++ app.py
@@ -3,21 +3,28 @@ from src.main import get_best_squad
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         team_id = request.form['team_id']
         game_week = request.form['game_week']
         wildcard = request.form.get('wildcard') == 'on'
-        
+
         try:
-            squad, best_11, captain, predicted_points = get_best_squad(int(team_id), int(game_week), wildcard)
-            return render_template('result.html', squad=squad, best_11=best_11, captain=captain, predicted_points=predicted_points)
+            squad, best_11, captain, predicted_points = get_best_squad(
+                int(team_id), int(game_week), wildcard)
+            return render_template('result.html',
+                                   squad=squad,
+                                   best_11=best_11,
+                                   captain=captain,
+                                   predicted_points=predicted_points)
         except Exception as e:
             error_message = str(e)
             return render_template('index.html', error=error_message)
-    
+
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
