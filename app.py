958b07da764e3fe8aabe0347ced0406e43f819c2
Akhil Varma <16632903+infinitetrooper@users.noreply.github.com>
2024-10-07 21:07:22 +0000

    Fixed timeout bug

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def index():
     return render_template('index.html', result=result, error=error)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, threaded=True, timeout=100)
+    app.run(host='0.0.0.0', port=80, threaded=True)
