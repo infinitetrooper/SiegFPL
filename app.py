d7d334d734e553bd4d0ad16b0b27051f7315c3a3
Akhil Varma <16632903+infinitetrooper@users.noreply.github.com>
2024-10-12 21:15:06 +0000

    Now printing Full Squad Cost

--- app.py
+++ app.py
@@ -35,15 +35,22 @@ def index():
         wildcard = request.form.get('wildcard') == 'on'
 
         try:
-            squad, best_11, captain, predicted_points, transfers = get_best_squad(
-                int(team_id), int(game_week), int(free_transfers), wildcard)
+            squad, best_11, captain, predicted_points, transfers = get_best_squad(int(team_id), int(game_week), int(free_transfers), wildcard)
             predicted_points = round(predicted_points)
+            total_cost = squad['value'].sum()
+            
+            # Sort squad and best_11 lists in order of GK, DEF, MID, FWD
+            position_order = {'GK': 1, 'DEF': 2, 'MID': 3, 'FWD': 4}
+            squad = squad.sort_values(by='position', key=lambda x: x.map(position_order.get))
+            best_11 = best_11.sort_values(by='position', key=lambda x: x.map(position_order.get))
+            
             result = {
                 'squad': squad,
                 'best_11': best_11,
                 'captain': captain,
                 'predicted_points': predicted_points,
-                'transfers': transfers
+                'transfers': transfers,
+                'total_cost': total_cost
             }
         except Exception as e:
             error = str(e)
